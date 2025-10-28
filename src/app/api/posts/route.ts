7cbe61b81635d97c5b025fe0b113763db193bc99
Dkay <kaludavid411@gmail.com>
2025-10-28 16:17:46 +0100

    new push

--- src/app/api/posts/route.ts
+++ src/app/api/posts/route.ts
@@ -1,11 +1,31 @@
 import { NextResponse } from "next/server";
 
-export async function GET() {
-    const posts = [
-        { id: 1, title: "Hello Next.js", body: "Welcome to the future of React apps." },
-        { id: 2, title: "API Routes Simplified", body: "How to use route handlers in Next.js." },
-        { id: 3, title: "Server Components FTW", body: "Server-first React architecture." },
-    ]
+const posts = [
+  { id: 1, title: "Hello Next.js", body: "Welcome to the future of React apps." },
+  { id: 2, title: "API Routes Simplified", body: "How to use route handlers in Next.js." },
+  { id: 3, title: "Server Components FTW", body: "Server-first React architecture." },
+];
 
-    return NextResponse.json(posts);
+// GET — read
+export async function GET() {
+  return NextResponse.json(posts);
+}
+
+// POST — create
+export async function POST(req: Request) {
+  try {
+    const data = await req.json(); // parse body
+    const newPost = {
+      id: posts.length + 1,
+      title: data.title,
+      body: data.body,
+    };
+
+    posts.push(newPost);
+
+    return NextResponse.json({ message: "Post created successfully!", post: newPost });
+  } catch (error) {
+    console.error("POST ERROR:", error);
+    return NextResponse.json({ error: "Invalid request" }, { status: 400 });
+  }
 }
---
+++ src/lib/mongodb.ts
@@ -0,0 +1,23 @@
+import mongoose from "mongoose";
+
+const MONGODB_URI = process.env.MONGODB_URI as string;
+
+//prevent multiple connections in development
+if (!MONGODB_URI) {
+    throw new Error("Please define the MONGODB_URI environment variable inside .env.local");
+}
+
+let isConnected = false
+
+export async function connectDB() {
+    if(isConnected) return;
+
+    try {
+        const db = await mongoose.connect(MONGODB_URI);
+        isConnected = !!db.connections[0].readyState;
+        console.log("MongoDB connected");
+    } catch(err) {
+        console.error("MongoDB connection failed:", err);
+        throw err
+    }
+}
